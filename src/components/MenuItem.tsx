62e73bbd313e6fed1564dcf2e8cacd2f25212d88
awallace77 <andrew77wallace@outlook.com>
2026-01-07 21:49:43 -0500

    Added vercel json

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -40,7 +40,7 @@ function MenuItem({
           src={img}
           alt={title}
           crossOrigin="anonymous"
-          className="w-24 h-full md:w-48 object-cover rounded-tl-lg rounded-bl-lg transition-transform duration-500 ease-in-out hover:scale-110"
+          className="w-28 h-full md:w-48 object-cover rounded-tl-lg rounded-bl-lg transition-transform duration-500 ease-in-out hover:scale-110"
         />
       </div>
 
